b2460f737745e9fbed15029a3aa4586c7984a4d3
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2023-06-20 15:08:34 +0300

    added an error property to the appReducer so that our component ErrorSnackbar.tsx can interact with the store

--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert, {AlertProps} from '@mui/material/Alert';
+import {useAppDispatch, useAppSelector} from "../../redux/store";
+import {setErrorAC} from "../../redux/reducers/app-reducer";
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
    props,
@@ -10,21 +12,21 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 });
 
 export const ErrorSnackbar = () => {
-   const [open, setOpen] = React.useState(true);
+   const error = useAppSelector(state => state.app.error);
+   const dispatch = useAppDispatch();
 
    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
-      console.log(reason)
       if (reason === 'clickaway') {
          return;
       }
 
-      setOpen(false);
+      dispatch(setErrorAC(null));
    };
 
    return (
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+      <Snackbar open={!!error} autoHideDuration={6000} onClose={handleClose}>
          <Alert onClose={handleClose} severity="error" sx={{width: '100%'}}>
-            This is a success message!
+            {error}
          </Alert>
       </Snackbar>
    );
--- src/redux/reducers/app-reducer.ts
+++ src/redux/reducers/app-reducer.ts
@@ -1,7 +1,8 @@
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed';
 
 const initialState = {
-   status: 'idle' as RequestStatusType
+   status: 'idle' as RequestStatusType,
+   error: null as null | string,
 }
 
 export type InitialStateType = typeof initialState;
@@ -14,20 +15,34 @@ export const AppReducer = (state: InitialStateType = initialState, action: AppRe
             status: action.status
          }
       }
+      case "SET_ERROR": {
+         return {
+            ...state,
+            error: action.error
+         }
+      }
       default: {
          return state;
       }
    }
 };
 
-export type AppRequestStatusType = SetRequestStatusType;
+export type AppRequestStatusType = SetRequestStatusType | SetErrorType;
 
 export type SetRequestStatusType = ReturnType<typeof setRequestStatus>;
+export type SetErrorType = ReturnType<typeof setErrorAC>;
 
 export const setRequestStatus = (status: RequestStatusType) => {
    return {
       type: 'APP/SET_STATUS',
       status,
+   } as const;
+};
+
+export const setErrorAC = (error: string | null) => {
+   return {
+      type: 'SET_ERROR',
+      error,
    } as const;
 }
 
